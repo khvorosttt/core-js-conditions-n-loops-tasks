75bb45011aa9e80a35df87a15805274ec1a95d32
khvorosttt <khvorosttt@yandex.by>
2023-12-24 01:50:53 +0300

    feat: add method implementation isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -92,8 +92,11 @@ function canQueenCaptureKing(/* queen, king */) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if ((a === b || a === c || b === c) && a < b + c && b < a + c && c < a + b) {
+    return true;
+  }
+  return false;
 }
 
 /**
