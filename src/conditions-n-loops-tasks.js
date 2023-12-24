fcff029dc0ffed528d5c3110638d7e6f2acf8cb3
khvorosttt <khvorosttt@yandex.by>
2023-12-25 02:09:43 +0300

    feat: add method implementation rotateMatrix

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -325,8 +325,15 @@ function getSpiralMatrix(/* size */) {
  *    [7, 8, 9]         [9, 6, 3]
  *  ]                 ]
  */
-function rotateMatrix(/* matrix */) {
-  throw new Error('Not implemented');
+function rotateMatrix(matrix) {
+  const newMatrix = matrix;
+  const initMatrix = JSON.parse(JSON.stringify(matrix));
+  for (let i = 0; i < matrix.length; i += 1) {
+    for (let j = 0; j < matrix[i].length; j += 1) {
+      newMatrix[j][matrix.length - 1 - i] = initMatrix[i][j];
+    }
+  }
+  return newMatrix;
 }
 
 /**
