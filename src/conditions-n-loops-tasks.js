362a08f16af66cd2e58fb8b97a2f21ed4b2d42bc
khvorosttt <khvorosttt@yandex.by>
2023-12-25 22:44:46 +0300

    feat: add method implementation convertToRomanNumerals

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -113,8 +113,50 @@ function isIsoscelesTriangle(a, b, c) {
  *  10  => X
  *  26  => XXVI
  */
-function convertToRomanNumerals(/* num */) {
-  throw new Error('Not implemented');
+
+function convertUnits(num) {
+  switch (num) {
+    case 1:
+      return 'I';
+    case 2:
+      return 'II';
+    case 3:
+      return 'III';
+    case 4:
+      return 'IV';
+    case 5:
+      return 'V';
+    case 6:
+      return 'VI';
+    case 7:
+      return 'VII';
+    case 8:
+      return 'VIII';
+    case 9:
+      return 'IX';
+    default:
+      return '';
+  }
+}
+
+function convertTens(num) {
+  switch (num) {
+    case 1:
+      return 'X';
+    case 2:
+      return 'XX';
+    case 3:
+      return 'XXX';
+    default:
+      return '';
+  }
+}
+
+function convertToRomanNumerals(num) {
+  if (num < 10) {
+    return convertUnits(num);
+  }
+  return convertTens(Math.floor(num / 10)) + convertUnits(num % 10);
 }
 
 /**
